eebe75231aea6080c48753db18d495cac3c771bb
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-11-24 23:54:51 -0500

    added the specific tostring from last project

--- app/src/main/java/com/example/rupizzeriaapp/Size.java
+++ app/src/main/java/com/example/rupizzeriaapp/Size.java
@@ -1,7 +1,20 @@
 package com.example.rupizzeriaapp;
 
+/**
+ * This is the enum class for the pizza sizes
+ * @author Varun Doreswamy, Yuet Yue
+ */
 public enum Size {
     SMALL(),
     MEDIUM(),
-    LARGE(),
+    LARGE();
+
+    /**
+     * This method converts the enums to the desired string format
+     * @return enum constant in a proper string format
+     */
+    @Override
+    public String toString() {
+        return name().charAt(0) + name().substring(1).toLowerCase();
+    }
 }
--- app/src/main/java/com/example/rupizzeriaapp/Topping.java
+++ app/src/main/java/com/example/rupizzeriaapp/Topping.java
@@ -15,4 +15,19 @@ public enum Topping {
     OLIVE(),
     JALEPENO(),
     HAM();
+
+    /**
+     * This method converts the enums to the desired string format
+     * @return enum constant in a proper string format
+     */
+    @Override
+    public String toString(){
+        return switch (name()){
+            case "GREENPEPPER" -> name().charAt(0) + name().substring(1,5).toLowerCase()
+                    + " " + name().charAt(5) + name().substring(6).toLowerCase();
+            case "BBQCHICKEN" -> name().substring(0,3) + " " + name().charAt(3)
+                    + name().substring(4).toLowerCase();
+            default -> name().charAt(0) + name().substring(1).toLowerCase();
+        };
+    }
 }
