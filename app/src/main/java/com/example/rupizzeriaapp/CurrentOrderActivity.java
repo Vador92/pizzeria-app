1409dcb3f6661418c8b5d12a8fb68f51a1387bd7
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-12-06 14:32:48 -0500

    added stuff for current order

--- app/src/main/java/com/example/rupizzeriaapp/CurrentOrderActivity.java
+++ app/src/main/java/com/example/rupizzeriaapp/CurrentOrderActivity.java
@@ -1,10 +1,19 @@
 package com.example.rupizzeriaapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CurrentOrderActivity extends AppCompatActivity {
+
+    private ListView currentCartList;
+    private EditText cartSubTotal, cartTax, cartTotal;
+    private Button removePizza, placeOrder, clearOrder;
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
